f287ece21a21864786678840776ca34188fcc0a2
José De La Paz <joseantonyodpf@gamil.com>
2025-02-08 21:37:03 -0500

    parche 4

--- docs/pages/0-registro/auth.js
+++ docs/pages/0-registro/auth.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL
+const API_URL = 'https://cultiva-en-casa-back.vercel.app/api/auth/'
 
 document.getElementById("registerForm").addEventListener("submit", async (e) => {
     e.preventDefault();
